c76d11fa5cc0eca0d877b2d9f0efc65d0327f8d5
rise4188@gmail.com <rise4188@gmail.com>
2023-11-16 18:46:08 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,3 @@
+const parent =  React.createElement(div, {id: "parent", React.createElement(div, {id:"child"}, React.createElement("h1", {id:heading, xyz:"abc"},"Helo world from react"))},)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
